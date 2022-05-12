8f2877a624a82908efa09ddc60bcc2426c3635b4
Mathieusc <mathieu.schweitzer@hotmail.fr>
2022-05-12 12:17:07 +0200

    locust performance tests for each endpoints

---
+++ tests/performance_tests/locustfile.py
@@ -0,0 +1,30 @@
+from locust import HttpUser, task
+
+
+class ProjectPerfTest(HttpUser):
+    @task
+    def home(self):
+        self.client.get("/")
+
+    @task
+    def logout(self):
+        self.client.get("/logout")
+
+    @task
+    def show_summary(self):
+        self.client.post("/showSummary", {"email": "john@simplylift.co"})
+
+    @task
+    def display_board(self):
+        self.client.get("/displayBoard")
+
+    @task
+    def display_competitions(self):
+        self.client.get("/book/Fall Classic/Simply Lift")
+
+    @task
+    def purchase_places(self):
+        self.client.post(
+            "/purchasePlaces",
+            {"places": "2", "club": "Simply Lift", "competition": "Fall Classic"},
+        )
